0dd9d38fd1625c716e1d4d4606b90d80b71a66a5
20rishika <107783153+20rishika@users.noreply.github.com>
2025-02-26 11:24:35 +0530

    Delete compare_predictions.py

--- compare_predictions.py
+++
@@ -1,6 +0,0 @@
-actual_synthesis_depth = 6
-predicted_depth = 5
-
-error = abs(actual_synthesis_depth - predicted_depth)
-print(f"\nActual Depth: {actual_synthesis_depth}, Predicted Depth: {predicted_depth}")
-print(f"Prediction Error: {error}")
